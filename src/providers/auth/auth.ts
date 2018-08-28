13a1d5511e8ac2160e5180ff7cb003018ac14995
Rivas <matias.rt@hotmail.com>
2018-08-28 08:36:56 -0400

    auth en firebase

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,7 +1,8 @@
 import { LoginPage } from './../../pages/login/login';
 import { Injectable } from '@angular/core';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
-import { App } from 'ionic-angular';
+import { App, ToastController } from 'ionic-angular';
+import firebase from 'firebase';
 
 /*
   Generated class for the AuthProvider provider.
@@ -13,17 +14,19 @@ import { App } from 'ionic-angular';
 export class AuthProvider {
 
   userData: any;
-  constructor(private fb: Facebook, public app: App) {
+  constructor(private fb: Facebook, public app: App, public toastCtrl: ToastController) {
     console.log('Hello AuthProvider Provider');
   }
 
   public login(): Promise<any> {
     return  this.fb.login(['public_profile', 'user_friends', 'email'])
-    .then((res: FacebookLoginResponse) => { this.fb.api('me?fields=id,name,email,picture.width(720).height(720).as(picture_large)', [])
-      .then(profile => { 
-        this.userData = {email: profile['email'], picture: profile['picture_large']['data']['url'], userName: profile['name']};
-      });
-    }).catch(e => console.log('Error logging into Facebook', e));
+    .then((res: FacebookLoginResponse) => {
+          var credential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+          firebase.auth().signInWithCredential(credential).then((info) => { this.showToast(info.displayName);});
+         }).then( () => { this.fb.api('me?fields=id,name,email,picture.width(720).height(720).as(picture_large)', []).then(profile => { 
+      this.userData = {email: profile['email'], picture: profile['picture_large']['data']['url'], userName: profile['name']};
+    })}
+    ).catch(e => console.log('Error logging into Facebook', e));
   }
 
   public logout(): void {
@@ -42,6 +45,15 @@ export class AuthProvider {
 
   public getUserData() : any {
     return this.userData;
+  }
+
+  showToast(name) {
+    const toast = this.toastCtrl.create({
+      message: 'Bienvenido ' + name,
+      position: 'bottom',
+      duration: 1000
+    });
+    toast.present();
   }
 
 }
